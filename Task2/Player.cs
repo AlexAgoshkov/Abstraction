db0a8f26b1746bd99647bcb3a0bfdcd2cbee2fe5
Alexey <alex.agoshkov6@gmail.com>
2019-12-14 16:17:46 +0200

    Task2
    Play and Record

---
+++ Task2/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Player : IPlayable, IRecodable
+    {
+        public void Record()
+        {
+            Console.WriteLine("Record was started");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Record Pause");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Record was stopped");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Song is playing");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Song pause");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Playing was stopped");
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayable playable = new Player();
+            IRecodable recodable = new Player();
+
+            playable.Play();
+            playable.Pause();
+            playable.Stop();
+
+            Console.WriteLine();
+
+            recodable.Record();
+            recodable.Pause();
+            recodable.Stop();
+
+
+            Console.ReadKey();
+        }
+    }
+}
